da1a2acd21bd5b35515ace272a7d693b83fd82f7
J-Segers <jethrosegers@gmail.com>
2024-08-14 19:46:42 +0200

    menu pagina toegevoegd waar menu word gelezen vanuit een json object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Home from './pages/home/Home';
+import Menu from './pages/menu/Menu';
+
 
 function App() {
+  var data = require("./data/menukaart_kam_po.json");
+
   return (
     <div className="App">
       <Home />
+      <Menu data={data} />
     </div>
   );
 }
---
+++ src/pages/menu/Menu.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import "./Menu.css";
+
+function Menu({data}) {
+
+    var menuCatArr = new Array();
+    
+    Object.keys(data.menu).forEach(key => {
+        menuCatArr.push(data.menu[key])
+    });
+    
+    return ( 
+        <div id="menu">
+            <h1>{data.restaurant}</h1>
+            <h2>Afhaal menu</h2>
+            {   
+                menuCatArr.map((cat) => {                    
+                    var menuItems = new Array();
+                    Object.entries(cat).forEach((items) => {
+                        console.log(cat);
+                        
+                        Object.values(items[1]).forEach((item) => {
+                            menuItems.push(item)
+                        })                       
+                    })
+                    return(
+                        <div className="menu-categorie">
+                            <h3 className="title">{Object.keys(cat)[0]}</h3>
+                            {
+                                menuItems.map((item) => {
+                                    return(
+                                        <div className="menu-item">
+                                            <div className="menu-nummer">{item.menuNummer}</div>
+                                            <div className="menu-gerecht">{item.gerechtNaam}</div>
+                                            <div className="menu-prijs">{item.prijs}</div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                })
+            }
+        </div>
+     );
+}
+
+export default Menu;
